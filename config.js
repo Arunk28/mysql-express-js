33f3d642a6384d014a916df74dd5de84b3bb0875
arunk28 <arunkumar280491@gmail.com>
2020-06-02 22:27:42 +0530

    add delete api

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
-    host     : 'localhost',
-  user     : 'newuser',
-  password : '123456789',
-  database : 'examonline',
-  insecureAuth : true
-}
+  host: "localhost",
+  user: "newuser",
+  password: "123456789",
+  database: "examonline",
+  insecureAuth: true,
+};
 module.exports = config;
--- index.js
+++ index.js
@@ -1,45 +1,64 @@
-var express  = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
-var app=  new express();
+var app = new express();
 app.use(cors());
-var mysql      = require('mysql');
-var config = require('./config')
+var mysql = require("mysql");
+var config = require("./config");
 //var connection = mysql.createConnection(config);
-app.get('/',(req,res)=>{    
-var connection = mysql.createConnection(config);
+app.get("/", (req, res) => {
+  var connection = mysql.createConnection(config);
 
-connection.connect();
-connection.query('SELECT * FROM examonline.userdata', function (error, results, fields) {
-    console.log(results);
-  if (error) console.log(error);
-  res.send( JSON.stringify(results));
-  connection.end();
-});
-
-
-
-});
-bodyParser = require('body-parser').json();
-app.post('/api/signup',bodyParser,(req,res)=>{
-    //normal validations    
-    if(!req.body.name && !req.body.password && !req.body.picture)
-    {
-        res.status(400).send("400 bad request");
+  connection.connect();
+  connection.query(
+    "SELECT * FROM examonline.userdata order by 1 desc",
+    function (error, results, fields) {
+      console.log(results);
+      if (error) console.log(error);
+      res.send(JSON.stringify(results));
+      connection.end();
     }
-    var connection = mysql.createConnection(config);
-    connection.connect();
-    const data ={name : req.body.name,password : req.body.password,picture:req.body.picture}
+  );
+});
 
-    let sql = `CALL InsertData("${data.name}","${data.password}","${data.picture}")`;
+app.delete("/api/delete/:id", (req, res) => {
+  var connection = mysql.createConnection(config);
 
-    connection.query(sql, true, (error, results, fields) => {
-    if (error) {return console.error(error.message);}
+  connection.connect();
+  connection.query(
+    "DELETE FROM examonline.userdata WHERE id = " + req.params.id,
+    function (error, results, fields) {
+      console.log(results);
+      if (error) console.log(error);
+      res.send(JSON.stringify(results));
+      connection.end();
+    }
+  );
+});
+
+bodyParser = require("body-parser").json();
+app.post("/api/signup", bodyParser, (req, res) => {
+  //normal validations
+  if (!req.body.name && !req.body.password && !req.body.picture) {
+    res.status(400).send("400 bad request");
+  }
+  var connection = mysql.createConnection(config);
+  connection.connect();
+  const data = {
+    name: req.body.name,
+    password: req.body.password,
+    picture: req.body.picture,
+  };
+
+  let sql = `CALL InsertData("${data.name}","${data.password}","${data.picture}")`;
+
+  connection.query(sql, true, (error, results, fields) => {
+    if (error) {
+      return console.error(error.message);
+    }
     res.send(results);
     connection.end();
-    });
-    
-    
+  });
 });
-const port= process.env.PORT  || 3000
-app.listen(port,()=>console.log(`am on at ${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`am on at ${port}`));
